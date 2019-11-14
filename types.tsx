b805d690e36498d8c2c8fa92ee857d6845ac7e95
zhangyanling77 <18783226594@163.com>
2019-11-14 15:16:34 +0800

    Create types.tsx

---
+++ types.tsx
@@ -0,0 +1,10 @@
+import { ClassComponent } from './react';
+
+export interface ReactElement {
+    type: string | FunctionComponent | ClassComponent,
+    props: any
+}
+
+export interface FunctionComponent {
+    (props: any): ReactElement
+}
