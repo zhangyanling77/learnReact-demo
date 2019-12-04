c7f165db580ff94c9c7d22c0c0775b356613afb9
zhangyanling77 <18783226594@163.com>
2019-12-04 09:36:08 +0800

    Update NavHeader.tsx

--- react-router/src/components/NavHeader.tsx
+++ react-router/src/components/NavHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RouteComponentProps } from '../react-router-dom';
 import { withRouter } from '../react-router-dom';
-//只有当一个组件是通过路由Route渲染出来的话才会有RouteComponentProps里的属性
+
 interface NavHeaderProps {
     title: string;
 }
