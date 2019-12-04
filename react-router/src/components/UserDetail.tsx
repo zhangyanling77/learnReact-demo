35d0104c7ea04d7be4632279671be47af84c6a52
zhangyanling77 <18783226594@163.com>
2019-12-04 09:38:33 +0800

    Update UserDetail.tsx

--- react-router/src/components/UserDetail.tsx
+++ react-router/src/components/UserDetail.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Link, RouteComponentProps } from '../react-router-dom';
 import { User } from '../types';
-//路径参数  path=/user/detail/:id/:name url=/user/detail/111/222 params={id:'111',name:'222'}
+
 interface Params {
     id: string;
 }
-//props.match.params.id;  match就是路径和path 匹配结果
+
 type Props = RouteComponentProps<Params>;
 
 interface State {
@@ -17,7 +17,6 @@ export default class extends Component<Props, State> {
         this.state = { user: {} as User };
     }
     componentDidMount() {
-        //先尝试从location的state中取值
         let user: User = this.props.location.state;
         if (!user) {
             let usersString = localStorage.getItem('users');
