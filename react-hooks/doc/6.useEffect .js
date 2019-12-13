ee050aaac5f4a15583d29ac76b55fb65eff12318
zhangyanling77 <18783226594@163.com>
2019-12-13 09:42:46 +0800

    refactor: tree shaking

--- react-hooks/doc/6.useEffect .js
+++ react-hooks/doc/6.useEffect .js
@@ -1,25 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-/* class Counter extends React.Component {
-    state = { number: 0 }
-    componentDidMount() {
-        document.title = this.state.number;
-    }
-    componentDidUpdate() {
-        document.title = this.state.number;
-    }
-    render() {
-        return (
-            <div>
-                <p>{this.state.number}</p>
-                <button onClick={() => this.setState({ number: this.state.number + 1 })}>+</button>
-            </div>
-        )
-    }
-} */
+
 function Counter() {
     let [state, setState] = useState({ number: 0 });
-    //useEffect里的函数会在组件挂载完成后，或者组件更新完成后进行调用
     useEffect(() => {
         document.title = state.number;
     });
@@ -30,20 +13,19 @@ function Counter() {
         </div>
     )
 }
-//跳过effect进行性能优化
-//Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+// 跳过effect进行性能优化
 function Counter2() {
     let [state, setState] = useState({ number: 0 });
-    //useEffect里的函数会在组件挂载完成后，或者组件更新完成后进行调用
-    //如果没有给第二个参数，那么函数会在每次执行渲染后调用
+    // useEffect里的函数会在组件挂载完成后，或者组件更新完成后进行调用
+    // 如果没有给第二个参数，那么函数会在每次执行渲染后调用
     useEffect(() => {
-        console.log('开始一个定时器');
+//         console.log('开始一个定时器');
         let timer = setInterval(() => {
             setState(x => ({ number: x.number + 1 }));
         }, 1000);
-        //useEffect会返回一个清理函数，当组件将要卸载的时候会执行清理函数
+        // useEffect会返回一个清理函数，当组件将要卸载的时候会执行清理函数
         return () => {
-            console.log('销毁一个定时器');
+//             console.log('销毁一个定时器');
             clearInterval(timer);
         }
     }, []);
