07b2d29584337be8a541a15948c928c359916774
zhangyanling77 <18783226594@163.com>
2019-12-13 09:39:53 +0800

    refactor：tree shaking

--- react-hooks/doc/2.useMemo.js
+++ react-hooks/doc/2.useMemo.js
@@ -2,21 +2,21 @@ import React, { useState, memo, useCallback, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 
 function Child(props) {
-    console.log('render Child');
+//     console.log('render Child');
     return (
         <button onClick={props.addClick}>{props.data.number}</button>
     )
 }
-//就让函数组件拥有了记忆的功能，只有当组件的属性发生变更的时候才会刷新，否则不刷新
-Child = memo(Child);
+
+Child = memo(Child); // 缓存，记忆
 let lastAddClick;
 let lastData;
 function App() {
     let [number, setNumber] = useState(0);
-    let [name, setName] = useState('zhufeng');
-    //第一个参数deps,表示此函数缓存依赖的变量，如果变量变了,会重新生成函数，
+    let [name, setName] = useState('hello hooks');
+    
     const addClick = useCallback(() => setNumber(number + 1), [number]);
-    console.log('lastAddClick === addClick', lastAddClick === addClick);
+//     console.log('lastAddClick === addClick', lastAddClick === addClick); // 每次都是一个新的引用地址，永远为false
     lastAddClick = addClick;
     const data = useMemo(() => ({ number }), [number]);
     console.log('lastData === data', lastData === data);
